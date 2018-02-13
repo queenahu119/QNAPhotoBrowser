61a4a779d323d2c1e37a8a1fc762666552ac769d
QueenaHuang <cindy119xyz@gmail.com>
2018-02-14 08:27:05 +1100

    Create the project

---
+++ QNAPhotoBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QNAPhotoBrowser
+//
+//  Created by QueenaHuang on 14/2/18.
+//  Copyright © 2018 queenahu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QNAPhotoBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QNAPhotoBrowser
+//
+//  Created by QueenaHuang on 14/2/18.
+//  Copyright © 2018 queenahu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
