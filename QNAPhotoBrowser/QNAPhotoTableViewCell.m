fcc94b355ff2a2edd7c51153a95e5022dfa41881
QueenaHuang <cindy119xyz@gmail.com>
2018-02-14 20:30:33 +1100

    Set Auto Layout on tableViewCell and change tableViewCell's layout

--- QNAPhotoBrowser/QNAPhotoTableViewCell.m
+++ QNAPhotoBrowser/QNAPhotoTableViewCell.m
@@ -7,6 +7,7 @@
 //
 
 #import "QNAPhotoTableViewCell.h"
+#import "Masonry.h"
 
 @implementation QNAPhotoTableViewCell
 
@@ -16,6 +17,32 @@
     [self.descriptionTextView setScrollEnabled:NO];
     [self.descriptionTextView setEditable:NO];
     [self.descriptionTextView setSelectable:NO];
+
+    [self.photoImageView setContentMode:UIViewContentModeScaleAspectFit];
+
+    UIEdgeInsets padding = UIEdgeInsetsMake(10, 10, -10, -10);
+
+    [self.photoImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.contentView.mas_left).offset(padding.left);
+        make.right.equalTo(self.contentView.mas_right).offset(padding.right);
+        make.top.equalTo(self.contentView.mas_top).offset(10);
+        make.height.equalTo(@150);
+    }];
+
+    [self.titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.contentView.mas_left).offset(padding.left);
+        make.right.equalTo(self.contentView.mas_right).offset(padding.right);
+        make.top.equalTo(self.photoImageView.mas_bottom).offset(padding.top);
+        make.height.equalTo(@(CGRectGetHeight(self.titleLabel.frame)));
+    }];
+
+    [self.descriptionTextView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.contentView.mas_left).offset(padding.left);
+        make.right.equalTo(self.contentView.mas_right).offset(padding.right);
+        make.top.equalTo(self.titleLabel.mas_bottom);
+        make.bottom.equalTo(self.contentView.mas_bottom).offset(padding.bottom);
+    }];
+
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
